be3c7967cc29d083eadf5460c9441df6df00784a
islandsaying <47809373+islandsaying@users.noreply.github.com>
2019-08-06 22:44:49 +0800

    创建官网

---
+++ docs/.vuepress/components/button-demos.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <g-button>默认按钮</g-button>
+        <g-button icon="settings">默认按钮</g-button>
+        <g-button :loading="true">默认按钮</g-button>
+        <g-button disabled>默认按钮</g-button>
+    </div>
+</template>
+<script>
+import Button from '../../../src/button'
+export default {
+    components: {
+        'g-button': Button
+    }
+}
+</script>
+<style lang="less">
+
+</style>
+
---
+++ docs/components/button.md
@@ -0,0 +1,21 @@
+---
+title: Button
+---
+
+# 按钮
+
+## 简单用法
+
+#### 预览
+<br>
+<button-demos></button-demos>
+
+#### 代码
+<br>
+
+```
+<g-button>默认按钮</g-button>
+<g-button icon="settings">默认按钮</g-button>
+<g-button :loading="true">默认按钮</g-button>
+<g-button disabled>默认按钮</g-button>
+```
