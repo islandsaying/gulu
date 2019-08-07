dc7eeb303a995f888a22e028db981b65827dd53e
islandsaying <47809373+islandsaying@users.noreply.github.com>
2019-08-07 14:43:04 +0800

    row 和 col 的雏形完成 col样式未解决

---
+++ src/col.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "GuluCol",
+  props: {
+    span: {
+      type: [Number, String]
+    }
+  }
+};
+</script>
+<style scoped lang="less">
+.col {
+  height: 100px;
+  background: grey;
+  width: 50%;
+  border: 1px solid red;
+
+//   $class: col-;
+//   @for $n from 1 through 24 {
+//     &.#{$class}#{$n} {
+//       width: ($n / 24) * 100%;
+//     }
+//   }
+//   @class: col-;
+//   .col-loop (@i) when (@i > 0){
+//       &.{@class}-@{i} {
+//           width: (@i / 24) * 100%;
+//       }
+//       .col-loop(@i - 1);
+//   }
+// }
+// .generate-columns(4);
+
+// .generate-columns(@n, @i: 1) when (@i =< @n) {
+//   .column-@{i} {
+//     width: (@i * 100% / @n);
+//   }
+//   .generate-columns(@n, (@i + 1));
+// }
+</style> 
