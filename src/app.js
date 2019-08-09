49e76a071eba26c92ae4496f528647ffbade2dae
islandsaying <47809373+islandsaying@users.noreply.github.com>
2019-08-09 16:48:58 +0800

    基本功能完成

--- src/app.js
+++ src/app.js
@@ -5,6 +5,12 @@ import ButtonGroup from './button-group'
 import Input from './input'
 import Row from './row'
 import Col from './col'
+import Layout from './layout'
+import Header from './header'
+import Footer from './footer'
+import Sider from './sider'
+import Content from './content'
+
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -12,6 +18,12 @@ Vue.component('g-button-group', ButtonGroup)
 Vue.component('g-input',Input)
 Vue.component('g-row', Row)
 Vue.component('g-col', Col)
+Vue.component('g-layout',Layout)
+Vue.component('g-header',Header)
+Vue.component('g-sider',Sider)
+Vue.component('g-footer',Footer)
+Vue.component('g-content',Content)
+
 
 new Vue({
   el: "#app",
--- src/footer.vue
+++ src/footer.vue
@@ -1,3 +1,11 @@
 <template>
-    
+    <div class="footer">
+        <slot></slot>
+    </div>
 </template>
+
+<style lang="less" scoped>
+    .footer{
+        
+    }
+</style>
