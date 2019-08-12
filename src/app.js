da7f78d53272a72b38965089bae32490b068f01b
islandsaying <47809373+islandsaying@users.noreply.github.com>
2019-08-12 22:54:45 +0800

    解决当容器有 overflow hidden 造成的 bug

--- src/app.js
+++ src/app.js
@@ -54,9 +54,9 @@ new Vue({
     
   },
   methods:{
-      yyy(data){
+      yyy(){
         console.log('yyy')
-        console.log(data)
+        
       },
       showToast1(){
         this.showToast('top')
