2e7695a99a037f95be3e6642d780ec5c9e6f0404
islandsaying <47809373+islandsaying@users.noreply.github.com>
2019-08-12 18:39:25 +0800

    触发 update:selected 事件的时候添加一个 item 数据

--- src/tabs.vue
+++ src/tabs.vue
@@ -1,43 +1,49 @@
 <template>
-    <div class="tabs">
-        <slot><slot>
-    </div>
+  <div class="tabs">
+    <slot></slot>
+  </div>
 </template>
 
 <script>
-import Vue from 'vue'
+import Vue from "vue";
 export default {
-    name: 'GuluTabs',
-    props: {
-        selected: {
-            type: String,
-            required: true
-        },
-        direction: {
-            type: String,
-            default: 'horizontal',
-            validator(value){
-                return ['horizontal','vertical'].indexOf(value) >= 0
-            }
-        }
+  name: "GuluTabs",
+  props: {
+    selected: {
+      type: String,
+      required: true
     },
-    data () {
-        return {
-            eventBus: new Vue()
-        }
-    },
-    provide() {
-        return {
-            eventBus: this.eventBus
-        }
-    },
-    mounted () {
-        // this.$emit('update:selected', '这是 this $emit 出来的数据')
-        this.eventBus.$emit('update:selected', this.selected)
-        // this.$emit('update:selected', 'xxx')
+    direction: {
+      type: String,
+      default: "horizontal",
+      validator(value) {
+        return ["horizontal", "vertical"].indexOf(value) >= 0;
+      }
     }
+  },
+  data() {
+    return {
+      eventBus: new Vue()
+    }
+  },
+  provide() {
+    return {
+      eventBus: this.eventBus
+    }
+  },
+  mounted() {
+    this.$children.forEach((vm) => {
+      if (vm.$options.name === "GuluTabsHead") {
+        vm.$children.forEach(childVm => {
+          if (childVm.$options.name === "GuluTabsItem" &&
+            childVm.name === this.selected) {
+            this.eventBus.$emit("update:selected", this.selected, childVm);
+          }
+        })
+      }
+    })
+  }
 }
 </script>
 <style >
-
 </style>
